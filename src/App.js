2a7981010d5cec2628d35f13480b2fb8b49c3d41
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-01 22:34:12 -0500

    add setting button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings, FieldSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import './App.css'
 
 export const App = () => {
   return (
-    <h1 className="underline text-3xl">App</h1>
+    <div>
+      <BrowserRouter>
+        <div className="flex realative dark:bg-amin-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className="text-3xl p-3 hover:dropshadow-xl hover:bg-light-gray text-white" style={{ background: 'blue', borderRadius: '50%' }}>
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
