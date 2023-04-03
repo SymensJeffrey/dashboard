a81d366f1ea1b566637ce54fc3ada7315d448da7
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-02 20:13:34 -0500

    Work on sidebar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 import './App.css'
 
 export const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
