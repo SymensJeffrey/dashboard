29cd8ca7de2a20117d5d50ec6cd55225455d521c
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-03 20:19:16 -0500

    Add context

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Line, Financial, ColorMapping, ColorPicker, Editor } from './pages'
+
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css'
 
 export const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+
   return (
     <div>
       <BrowserRouter>
