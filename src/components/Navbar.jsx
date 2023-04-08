cdb4965856191082b31493d7eb64e6a089248f5c
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-07 21:48:50 -0500

    add all icons to navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,57 @@ const Navbar = () => {
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
-      <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick('cart')}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick('chat')}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+        <NavButton
+          title="Notifications"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick('notification')}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent
+          content="Profile"
+          position="BottomCenter"
+        >
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick('userProfile')}
+          >
+            <img
+              className="rounded-full w-8 h-8"
+              src={avatar}
+              alt=""
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span> {' '}
+              <span className="text-gray-400 font-bold ml-1 text-14"> Michael</span>
+            </p>
+            <MdKeyboardArrowDown
+              className="text-gray-400 text-14"
+            />
+          </div>
+
+        </TooltipComponent>
+      </div>
 
     </div >
   )
