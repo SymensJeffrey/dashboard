4076b6ff90713fa8965a243be07dc33bdc61c311
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-08 21:43:59 -0500

    Add data to ecommerace section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,20 @@
-import { Button } from '@syncfusion/ej2/buttons'
-import React from 'react'
+import React from 'react';
 
-export const button = () => {
+import { useStateContext } from '../contexts/ContextProvider';
+
+const Button = ({ icon, bgColor, color, bgHoverColor, size, text, borderRadius, width }) => {
+  const { setIsClicked, initialState } = useStateContext();
+
   return (
-    <div>button</div>
-  )
-}
+    <button
+      type="button"
+      onClick={() => setIsClicked(initialState)}
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
+    >
+      {icon} {text}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
