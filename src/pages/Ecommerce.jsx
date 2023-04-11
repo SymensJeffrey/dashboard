edd936946805b6ca0483b3e484368f237eed3423
symensjeffrey <jeffrey.symens23@gmail.com>
2023-04-10 21:57:23 -0500

    fix bg image url

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -9,7 +9,7 @@ import { useStateContext } from '../contexts/ContextProvider'
 const Ecommerce = () => {
   return (
     <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
